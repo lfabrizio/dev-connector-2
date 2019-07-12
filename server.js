04adddd40d6ebe4bfe924aaf60e4cc291cd00e18
lfabrizio <louisfabrizio.student@careerdevs.com>
2019-07-12 09:53:16 -0400

    MongoDB Connected

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 const express = require('express');
+const connectDB = require('./config/db');
+
 const app = express();
+
+//connect datda base
+connectDB();
 
 app.get('/', (req, res) => res.send('API running'));
 const PORT = process.env.PORT || 5000;
