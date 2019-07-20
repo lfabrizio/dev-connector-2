b7193a01dd17538eab12989c2b556869786a83b1
lfabrizio <louisfabrizio.student@careerdevs.com>
2019-07-20 10:21:20 -0400

    intial components

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,6 @@
 import React, { Fragment } from 'react';
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
 import './App.css';
 
 const App = () => 
